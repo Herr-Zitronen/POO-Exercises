cf043b0a70d02d33963ca7c2e451e4692eb9f542
Herr-Zitronen <joelsacguerrero@proton.me>
2025-05-30 07:25:35 -0500

    Added parametrizacion

--- Circulo - Cilindro.cpp
+++ Circulo - Cilindro.cpp
@@ -5,7 +5,10 @@ class Punto {
 private:
     float x, y;
 public:
-    Punto(float x = 0, float y = 0) : x(x), y(y) {}
+    // Constructor por defecto y parametrizado.
+
+    Punto() : x(0), y(0) {}
+    Punto(float x, float y) : x(x), y(y) {}
 
     float getX() const { return x; }
     float getY() const { return y; }
@@ -16,26 +19,27 @@ public:
 
 class Circulo : public Punto {
 private:
-    float radio;
+    float r;
 public:
-    Circulo(float x = 0, float y = 0, float radio = 0) : Punto(x, y), radio(radio) {}
+    Circulo() : Punto(), r(0) {}
+    Circulo(float x, float y, float radio) : Punto(x, y), r(radio) {}
 
-    float getRadio() const { return radio; }
+    float getRadio() const { return r; }
 
-    void setRadio(float radio) { this->radio = radio; }
+    void setRadio(float radio) { r = radio; }
 };
 
 class Cilindro : public Circulo {
 private:
-    float altura;
+    float h;
 public:
-
-    Cilindro(float x = 0, float y = 0, float radio = 0, float altura = 0)
-        : Circulo(x, y, radio), altura(altura) {}
+    Cilindro() : Circulo(), h(0) {}
+    Cilindro(float x, float y, float radio, float altura)
+        : Circulo(x, y, radio), h(altura) {}
     
     
-    float getAltura() const { return altura; }
-    void setAltura(float altura) { this->altura = altura; }
+    float getAltura() const { return h; }
+    void setAltura(float altura) { h = altura; }
 };
 
 int main(){
